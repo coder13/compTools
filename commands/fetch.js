5c58d89562246dfd7ba7e769ef1cc1ac90fd61ee
Caleb <choover11@gmail.com>
2020-03-03 07:56:12 -0800

    added fetch

---
+++ commands/fetch.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const api = require('../lib/api');
+
+exports.command = 'fetch';
+exports.desc = 'Fetches WCIF';
+
+exports.builder = {
+  output: {
+    alias: 'o',
+    description: 'Output file',
+    normalize: true,
+  },
+};
+
+exports.handler = (argv) => {
+  const fetch = argv.auth ? api.fetchWcif(argv.comp, argv.auth) : api.fetchPublicWcif(argv.comp);
+
+  fetch
+    .then((res) => {
+      const dest = argv.output ? fs.createWriteStream(argv.output)
+        : process.stdout;
+
+      res.body.pipe(dest);
+    });
+};
